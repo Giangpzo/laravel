c024773d45ff1e0d15390b6d27bc1211a5398f22
Giangpzo <giangpzo@gmail.com>
2023-03-24 16:29:45 +0700

    Add maintenance page

--- makefiles/artisan.mk
+++ makefiles/artisan.mk
@@ -4,3 +4,12 @@ migrate:
 
 clear-cache:
 	sudo docker compose -f ./docker-composes/docker-compose.yaml -f ./docker-composes/docker-compose.artisan.yaml run --rm artisan cache:clear
+
+maintenance-on:
+	sudo docker compose -f ./docker-composes/docker-compose.yaml -f ./docker-composes/docker-compose.artisan.yaml run --rm artisan down --render="maintenance"
+
+maintenance-off:
+	sudo docker compose -f ./docker-composes/docker-compose.yaml -f ./docker-composes/docker-compose.artisan.yaml run --rm artisan up
+
+artisan:
+	sudo docker compose -f ./docker-composes/docker-compose.yaml -f ./docker-composes/docker-compose.artisan.yaml run --rm --entrypoint='' artisan sh
