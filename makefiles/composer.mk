3d3cb859fbd47ff10e4661378b278e80910421d5
Giangpzo <giangpzo@gmail.com>
2023-03-25 21:54:09 +0700

    Make composer

--- makefiles/composer.mk
+++ makefiles/composer.mk
@@ -1,2 +1,5 @@
 init-code:
 	sudo docker compose -f ./docker-composes/docker-compose.yaml -f ./docker-composes/docker-compose.composer.yaml run --rm composer create-project laravel/laravel .
+
+composer:
+	sudo docker compose -f ./docker-composes/docker-compose.yaml -f ./docker-composes/docker-compose.composer.yaml run --rm --entrypoint='' composer sh
